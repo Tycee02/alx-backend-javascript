efb618de929c91a45452a0ccf5da3475f1f12ad3
Tycee02 <wakilaminat@gmail.com>
2024-09-18 06:42:41 +0100

    file

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display a string message on the STDOUT
+ * @param {String} message: The message to display.
+ */
+
+function displayMessage(message) {
+	console.log(message);
+}
+
+module.exports = displayMessage;
